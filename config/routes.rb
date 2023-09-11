87174f4ea3d70b32b03414185bb5c516ed1dff1c
alicefirminger <alicefirminger@gmail.com>
2023-09-11 15:34:44 +0100

    created a form that will create a new article

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
   # Defines the root path route ("/")
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
  
 end
