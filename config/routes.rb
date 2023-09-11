2d4efef5a08de7b22986e5bd14c837e119c5559f
alicefirminger <alicefirminger@gmail.com>
2023-09-11 14:48:25 +0100

    linked each article by id to be rendered on screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "articles#index"
   # Defines the root path route ("/")
   get "/articles", to: "articles#index"
-  
+  get "/articles/:id", to: "articles#show"
  
 end
